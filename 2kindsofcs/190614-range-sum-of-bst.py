6557672edc9be73f0ce0f4be0cc96d17d4be2aa0
2kindsofcs <2kindsofcs@gmail.com>
2019-06-14 15:50:46 +0900

    improve quality of code

--- 2kindsofcs/190614-range-sum-of-bst.py
+++ 2kindsofcs/190614-range-sum-of-bst.py
@@ -15,18 +15,17 @@ class Solution:
                 isSmall = root.val < L
                 isBig = root.val > R
                 if not isSmall and not isBig:
-                    sum += root.val
-                    sum += preOrder(root.right, L, R)                    
-                    sum += preOrder(root.left, L, R)
+                    sum += root.val + preOrder(root.right, L, R) + preOrder(root.left, L, R)
                 elif isSmall:
                     sum += preOrder(root.right, L, R)
-                elif isBig:
+                else:
                     sum += preOrder(root.left, L, R)
             return sum 
         return preOrder(root, L, R)
 
-# Runtime: 236 ms, faster than 64.77% of Python3 online submissions for Range Sum of BST.
-# Memory Usage: 22 MB, less than 5.20% of Python3 online submissions for Range Sum of BST.
+# Runtime: 208 ms, faster than 99.60% of Python3 online submissions for Range Sum of BST.
+# Memory Usage: 21.8 MB, less than 29.73% of Python3 online submissions for Range Sum of BST.
 
 # 각 노드의 값은 고유하다고 했고, BST 특성을 이용하면 노드 값이 L보다 작다면 해당 노드의 왼쪽 자식은 애초에 검사할 필요가 없음.
-# 반대로 오른쪽은 R보다 크면 오른쪽 자식을 검사할 필요가 없음. 이를 활용해서 조금 고쳐보았는데, 속도는 조금 빨라졌지만 코드가 썩 맘에 들지는 않는다.
+# 마지막 elif isBig을 else로 수정. isBig을 확인할 필요가 없다. 값이 L과 R 사이에 있는 것도 아니고, L보다 작은 것도 아니라면 R보다 큰 경우밖에 없기 때문이다.
+# 확실히 속도가 많이 향상되었다. 
