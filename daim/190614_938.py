5e990651018381fa48ebca1bd58dc91ebb0c1174
Daim Tak <histuckyi@gmail.com>
2019-06-14 15:39:20 +0900

    Modify 190614 leetcode While loop version

--- daim/190614_938.py
+++ daim/190614_938.py
@@ -6,13 +6,15 @@ Memory Usage : 21.9 MB, less than 8.39%
 """
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-class Solution:
+
+# Recursion
+class Solution1:
     def checkChildBST(self, root, result, l, r):
 
         # if leaf node, recursion must be closed
@@ -39,3 +41,35 @@ class Solution:
 
     def rangeSumBST(self, root, L: int, R: int) -> int:
         return self.checkChildBST(root, 0, L, R)
+
+
+# while loop
+class Solution2:
+    def rangeSumBST(self, root, L: int, R: int) -> int:
+
+        node_list = [root]
+        result = 0
+
+        while node_list:
+            current_node = node_list.pop()
+
+            if L < current_node.val < R:
+                result += current_node.val
+                if current_node.left:
+                    node_list.append(current_node.left)
+                if current_node.right:
+                    node_list.append(current_node.right)
+
+            elif current_node.val <= L:
+                if current_node.val == L:
+                    result += current_node.val
+                if current_node.right:
+                    node_list.append(current_node.right)
+
+            elif R <= current_node.val:
+                if R == current_node.val:
+                    result += current_node.val
+                if current_node.left:
+                    node_list.append(current_node.left)
+
+        return result
