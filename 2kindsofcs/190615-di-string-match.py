619af6eb82bf05598d2999abc034c03a361b08a5
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-06-15 23:48:54 +0900

    Improve quality of code 190615

--- 2kindsofcs/190615-di-string-match.py
+++ 2kindsofcs/190615-di-string-match.py
@@ -3,18 +3,17 @@ class Solution:
         N = len(S)
         Dnum, Inum = N , 0
         result = []
-        for i in range(0, N):
-            if S[i] == "D":
+        # 순서대로 한 문자씩 가져와서 판별하면 되므로 꼭 range 사용하지 않아도 됨
+        for letter in S:
+            if letter == "D":
                 result.append(Dnum)
                 Dnum = Dnum - 1
-            else:
+            else: # letter == "I"
                 result.append(Inum)
-                Inum += 1
-        if S[-1] == "D":
-            result.append(Dnum)
-        else:
-            result.append(Inum)
+                Inum = Inum + 1
+        # 판별하지 않고 바로 Inum을 뒤에 붙이고 끝냄
+        result.append(Inum) # 사실 Inum == Dnum인데 편의상 그냥 Inum 씀 
         return result
             
-# Runtime: 108 ms, faster than 25.91% of Python3 online submissions for DI String Match.
-# Memory Usage: 14.3 MB, less than 55.42% of Python3 online submissions for DI String Match.
+# Runtime: 88 ms, faster than 91.10% of Python3 online submissions for DI String Match.
+# Memory Usage: 14.2 MB, less than 67.40% of Python3 online submissions for DI String Match.
