9ab31cec83ba38b3e8e73dc5d49bf437fc257c49
2kindsofcs <2kindsofcs@gmail.com>
2019-06-18 17:40:16 +0900

    Remove str()

--- 2kindsofcs/190618-hamming-distance.py
+++ 2kindsofcs/190618-hamming-distance.py
@@ -1,10 +1,9 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         xorNum = bin(x ^ y)
-        return str(xorNum).count("1")
+        return xorNum[2:].count("1")
         
-
-# Runtime: 36 ms, faster than 78.07% of Python3 online submissions for Hamming Distance.
-# Memory Usage: 13 MB, less than 84.26% of Python3 online submissions for Hamming Distance.
+# Runtime: 32 ms, faster than 91.93% of Python3 online submissions for Hamming Distance.
+# Memory Usage: 13.1 MB, less than 60.69% of Python3 online submissions for Hamming Distance.
     
-# 일단 떠오르는대로 금방 풀었는데 결과는 생각보다 괜찮다. 다만 코드가 좀 불친절한 것 같다. 
+# bin 메소드의 리턴값은 스트링이다.
