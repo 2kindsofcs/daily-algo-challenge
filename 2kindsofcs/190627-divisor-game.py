9b277dbac88f89b813ab4e5c2b8269a123f60bc6
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-06-28 09:10:44 +0900

    Solve 190627 leetcode divisor-game

---
+++ 2kindsofcs/190627-divisor-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def divisorGame(self, N: int) -> bool:
+        if N % 2 == 0:
+            return True
+        return False
+    
+# Runtime: 32 ms, faster than 94.46% of Python3 online submissions for Divisor Game.
+# Memory Usage: 13.2 MB, less than 60.67% of Python3 online submissions for Divisor Game.
