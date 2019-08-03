4eb32994410e2a257e9f95b0de551a02a7f8ff72
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-08-03 13:52:37 +0900

    Solve 190801 rectangle problem in another way

--- 2kindsofcs/190801-construct-the-rectangle.py
+++ 2kindsofcs/190801-construct-the-rectangle.py
@@ -1,18 +1,16 @@
+import math
+
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
-        diff = 10000000
-        answer = [area, 1]
-        for i in range(1, area+1):
-            if i ** 2 > area:
-                break
-            if area % i == 0:
-                counterpart = area // i
-                if abs(i - counterpart) < diff:
-                    diff = abs(i - counterpart)
-                    answer[0] = max(i, counterpart)
-                    answer[1] = min(i, counterpart)
-        return answer
+        l = math.floor(math.sqrt(area))
+        if l * l == area:
+            return [l, l]
+        while l < area:
+            l += 1
+            if area % l == 0 and area % (area // l) == 0:
+                return [l, area // l]
 
-# Runtime: 40 ms, faster than 63.01% of Python3 online submissions for Construct the Rectangle.
-# Memory Usage: 13.7 MB, less than 6.52% of Python3 online submissions for Construct the Rectangle.
-# 소수에 대한 계산을 따로 뺄 수도 있지 않을까 싶기는 한데, 일단은 그냥 떠오르는대로 풀어보았다. 
+# Runtime: 1748 ms, faster than 5.38% of Python3 online submissions for Construct the Rectangle.
+# Memory Usage: 13.6 MB, less than 5.00% of Python3 online submissions for Construct the Rectangle.
+# hotsummer님 코드를 참고하여 바꿔보았는데, 코드 자체는 더 간결해진 것 같지만 메모리는 약간 덜 사용하고 속도는 배로 느려졌다.
+# math.sqrt의 비용이 생각보다 비싼가? 그래도 이전 코드에 있던 abs()가 빠졌는데.
