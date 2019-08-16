30f439d1b8e0004a211c4454436313dc587eeb35
sleewatermelon <dlthsohy@gmail.com>
2019-08-16 16:40:52 -0400

    Solve 190731 Best Time to Sell Stock - [ ] redo

---
+++ sohy/190731_best_time_to_sell_stock.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        maxProfit = 0;
+        minPriceSoFar = float('inf')
+        
+        for curPrice in prices: 
+            minPriceSoFar = min(curPrice, minPriceSoFar)
+            profit = curPrice - minPriceSoFar
+            maxProfit = max(maxProfit, profit)
+            
+        return maxProfit
+        
+
+        
+'''
+Runtime: 52 ms, faster than 38.01% of Python online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 12.6 MB, less than 35.78% of Python online submissions for Best Time to Buy and Sell Stock.
+
+- redo [ ]
+hard to understand the logic 
+
+'''
