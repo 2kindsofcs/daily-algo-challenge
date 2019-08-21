523780faf1aae544b35c46abf98392e82ea53c5d
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-08-21 20:18:19 +0900

    Update 190815-subtree-of-another-tree.py

--- 2kindsofcs/190815-subtree-of-another-tree.py
+++ 2kindsofcs/190815-subtree-of-another-tree.py
@@ -16,17 +16,18 @@ class Solution:
             while nodeList:
                 node = nodeList.pop()
                 # 단순히 s의 preorder 결과물 안에 t의 preorder의 결과물이 있는지를 확인할 경우
-                # s = [1,2,3] 이고 t = [2, 3] 같은 경우를 올바르게 판단할 수 없으므로 왼쪽 없는 자식과 오른쪽 없는 자식을 구분해서 표기 
-                if node and node != 'rNull' and node != 'lNull':
+                # s = [1,2,3] 이고 t = [2, 3] 같은 경우를 올바르게 판단할 수 없으므로 없는 자식도 없다고 표기를 해준다
+                # 오른쪽 없는 자식이든 왼쪽 없는 자식이든 결과에는 영향을 미치지 않으므로 수정 
+                if node and node != 'Null':
                     result.append(node.val)
                     if node.right:
                         nodeList.append(node.right)
                     else:
-                         result.append('rNull')
+                         result.append('Null')
                     if node.left:
                         nodeList.append(node.left)
                     else:
-                         result.append('lNull')
+                         result.append('Null')
             return result
         tList, sList = preorder(t), preorder(s)
         tLength, sLength = len(tList), len(sList)
