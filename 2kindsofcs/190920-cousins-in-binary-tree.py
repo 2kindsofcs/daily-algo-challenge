04aec9d3c9403106ce4740a63cae009b84cefeec
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-09-22 00:02:20 +0900

    Update 190920-cousins-in-binary-tree.py

--- 2kindsofcs/190920-cousins-in-binary-tree.py
+++ 2kindsofcs/190920-cousins-in-binary-tree.py
@@ -12,25 +12,26 @@ class Solution:
         q = queue.Queue()
         q.put(root)
         xP, yP = 0, 0
+        children = []
         while q:
             node = q.get()
             if node:
-                children = []
                 children.append(node.left)
                 children.append(node.right)
-                if x in children:
-                    xP = node.val
-                if y in children:
-                    yP = node.val
-                if xP and yP:
-                    if xP != yP:
+            if q.empty():
+                valueList = [ child.val for child in children ]
+                if x in valueList and y in valueList:
+                    xP = valueList.index(x) // 2
+                    yP = valueList.index(y) // 2
+                    if xP and yP and xP != yP:
                         return True
                     else:
                         return False
-            for child in children:
-                if child:
-                    q.put(child.left)
-                    q.put(child.right)
+                for child in children:
+                    if child:
+                        q.put(child.left)
+                        q.put(child.right)
+                children = []
         return False
-                
-# 같은 높이임을 어떻게 체크할 것인가?
+    
+# time limit exceeded가 뜬다. 어디선가 무한반복이 되고 있다는 것. 
