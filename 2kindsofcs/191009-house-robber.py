34127df547df660c1ba92aeb20e21f7f53e44dc6
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-10-12 17:02:23 +0900

    Solve 191009 leetcode house-robber

--- 2kindsofcs/191009-house-robber.py
+++ 2kindsofcs/191009-house-robber.py
@@ -1,12 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         length = len(nums)
-        def robMax(index, total):
-            if index >= length:
-                return total
-            total += nums[index]
-            return max(robMax(index + 2, total), robMax(index + 3, total))
-        return max(robMax(0,0), robMax(1,0))
-
+        if length == 0:
+            return 0
+        if length <= 2:
+            return max(nums)
+        if length == 3:
+            return max((nums[0] + nums[2]), nums[1])
+        maxList = [ 0 for _ in range(length) ]
+        maxList[0] = nums[0]
+        maxList[1] = nums[1]
+        maxList[2] = nums[0] + nums[2]
+        for i in range(3, length):
+            maxList[i] = nums[i] + max(maxList[i - 3], maxList[i - 2])
+        return max(maxList[length - 2], maxList[length - 1])
     
-# time limit exceeded
+    
+# Runtime: 40 ms, faster than 49.57% of Python3 online submissions for House Robber.
+# Memory Usage: 13.6 MB, less than 9.09% of Python3 online submissions for House Robber.
