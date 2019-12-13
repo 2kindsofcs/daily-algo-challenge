816af0c908a13a9fab4e2088672ba554717338c9
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-12-13 22:59:15 +0900

    Update 191213-fair-candy-swap.py

--- 2kindsofcs/191213-fair-candy-swap.py
+++ 2kindsofcs/191213-fair-candy-swap.py
@@ -1,14 +1,16 @@
 class Solution:
     def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
-        ALength = len(A)
-        BLength = len(B)
         Asum = sum(A)
         Bsum = sum(B)
+        A = set(A)
+        B = set(B)
         halfDiff = (Asum - Bsum) / 2
-        for i in range(BLength):
-            target = halfDiff + B[i]
+        for num in B:
+            target = halfDiff + num
             if target in A:
-                return [int(target), B[i]]
- 
+                return [int(target), num]
+
+# Runtime: 408 ms, faster than 80.23% of Python3 online submissions for Fair Candy Swap.
+# Memory Usage: 15.6 MB, less than 8.33% of Python3 online submissions for Fair Candy Swap.
         
- # O(n)으로 줄였지만 여전히 Time limit exceeded
+# 핵심은 크게 2가지였다. 최소한의 연산으로, 중복되는 요소 없이 확인하기.
