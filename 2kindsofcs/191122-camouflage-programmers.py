51695750ffee2257972d13ac73bf8f24ae98c543
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2020-01-01 13:25:14 +0900

    Update 191122-camouflage-programmers.py

--- 2kindsofcs/191122-camouflage-programmers.py
+++ 2kindsofcs/191122-camouflage-programmers.py
@@ -1,21 +1,15 @@
-import itertools
 def solution(clothes):
-    answer = 0
+    answer = 1
     clothesDic = {}
     for item in clothes:
         if item[1] not in clothesDic:
-            clothesDic[item[1]] = 1
+            clothesDic[item[1]] = 2
         else:
             clothesDic[item[1]] = clothesDic[item[1]] + 1
-    categories = clothesDic.keys()
-    categoryCount = len(categories)
-    for i in range(1, categoryCount + 1):
-        combiList = itertools.combinations(categories, i)
-        for combi in combiList:
-            result = 1
-            for category in combi:
-                result = result * clothesDic[category]
-            answer = answer + result
-    return answer  
-    
- # 1번 테스트케이스를 시간 초과로 통과하지 못하고 있다. 
+    counts = clothesDic.values()
+    for count in counts:
+        answer = answer * count
+    return answer - 1
+
+
+# 착용하지 않는 케이스까지 포함해서 계산한 다음 단 하나도 걸치지 않은 케이스 1개만 빼주면 간편하다. 
